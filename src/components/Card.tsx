d428e4bd78047b9624e97a24e3fe8c95b241d592
Ashish7107 <yadavashish7107@gmail.com>
2023-11-23 18:29:53 +0530

    dd

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,11 +11,7 @@ const Card = ({ heading, image, text }: Props) => {
   return (
     <div className="  grid grid-cols-1    max-w-xs ">
       <h1 className="text-blue-900 justify-center flex text-2xl ">{heading}</h1>
-      <img
-        src={image}
-        alt=""
-        className=" flex px-4 md:px-8 lg:px-16 xl:px-32 h-[80px]"
-      />
+      <img src={image} alt="" className=" flex px-32 h-[80px]" />
 
       <p className="text-sm  pt-16">{text}</p>
     </div>
